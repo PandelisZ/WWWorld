e94d695226553ae78f9fb3724905863ac40efb29
sprusr <scottystreet@gmail.com>
2016-08-07 10:25:19 -0400

    Fix issue with updating synced time
    All clients were attempting to update the time, now first user to load
    is 'master' and subsequent will be slaves.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,86 +1,71 @@
 const config = {
-  host: 'https://twilio.mattburman.com'
+  host: '//twilio.mattburman.com'
 };
 
 
 chrome.extension.sendMessage({}, function(response) {
-
-	/*var readyStateCheckInterval = setInterval(function() {
-  	if (document.readyState === "complete") {
-  		clearInterval(readyStateCheckInterval);
-
-  		// ----------------------------------------------------------
-  		// This part of the script triggers when page is done loading
-  		console.log("Hello. This message was sent from scripts/inject.js");
-  		// ----------------------------------------------------------
-
-
-
-  		// Editing the dom and manipulating the youtube
-  		var ytID = window.location.search;
-  		ytID = ytID.substring(2);
-
-  		var ytEmbed = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'+ ytId +'" frameborder="0" allowfullscreen></iframe>';
-
-  		var mmvStart = function(){
-
-  			$( "#body-container" ).append(ytEmbed);
-
-
-  		};
-
-  	}
-	}, 10);*/
-
   var readyStateCheckInterval = setInterval(function() {
-  if (document.readyState === "complete") {
-    clearInterval(readyStateCheckInterval);
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-    $('head').append("<script type='text/javascript' src='https://media.twiliocdn.com/sdk/js/common/v0.1/twilio-common.min.js'>");
-    $('head').append("<script type='text/javascript' src='https://media.twiliocdn.com/sdk/js/sync/v0.2/twilio-sync.min.js'>");
+      $('head').append("<script type='text/javascript' src='https://media.twiliocdn.com/sdk/js/common/v0.1/twilio-common.min.js'>");
+      $('head').append("<script type='text/javascript' src='https://media.twiliocdn.com/sdk/js/sync/v0.2/twilio-sync.min.js'>");
+      var client;
 
-    // ----------------------------------------------------------
-    // This part of the script triggers when page is done loading
-    console.log("Hello. This message was sent from scripts/inject.js");
-    // ----------------------------------------------------------
+      fetchAccessToken(function(data) {
+        client = new Twilio.Sync.Client(new Twilio.AccessManager(data.token));
 
-    //'user'=token
-    var client;
+        console.log("You are: " + client.accessManager.identity);
 
-    fetchAccessToken(function(data) {
-      console.log(data);
-      client = new Twilio.Sync.Client(new Twilio.AccessManager(data.token));
-
-      console.log($('video')[0]);
-      console.log(client);
-
-      client.document("vid").then(function (doc) {
-        doc.on("updated",function(data) {
-          $('video')[0].currentTime = data.time;
-        });
-      });
-
-      var ytCheck = setInterval(function() {
-        //console.log($('video')[0].currentTime);
-
-        client.document('vid').then(function(doc) {
+        client.document("vid").then(function (doc) {
           doc.mutate(function(data) {
-            data.time = $('video')[0].currentTime;
+            if(!data.master) {
+              data.master = client.accessManager.identity;
+              data.time = $('video')[0].currentTime;
+            } else {
+              $('video')[0].currentTime = data.time;
+            }
+
+            console.log("Master is: " + data.master);
+
             return data;
           });
-        });
-      }, 1000);
 
-    });
-  }
+          doc.on("updated",function(data) {
+            if(data.master != client.accessManager.identity) {
+              $('video')[0].currentTime = data.time;
+            }
+          });
+        });
+
+        $('video')[0].onplay = function() {
+          client.document('vid').then(function(doc) {
+            doc.mutate(function(data) {
+              if(data.master == client.accessManager.identity) {
+                data.time = $('video')[0].currentTime;
+              }
+
+              return data;
+            });
+          });
+        };
+
+        window.onbeforeunload = function (e) {
+          client.document('vid').then(function(doc) {
+            doc.mutate(function(data) {
+              data.master = null;
+
+              return data;
+            });
+          });
+        };
+      });
+    }
   }, 10);
 });
 
 function fetchAccessToken (cb) {
-    $.getJSON(config.host + '/token', { device: 'browser' }, data => {
-      console.log(data.token);
-      console.log(data.identity);
-
-      cb(data);
-    });
+  $.getJSON(config.host + '/token', { device: 'browser' }, data => {
+    cb(data);
+  });
 }
