2574af8d4c2b903f08364439684df01238d60c4b
Pandelis Zembashis <info@pandelis.me>
2016-08-07 08:07:43 -0400

    added actual stuff

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -8,6 +8,21 @@ chrome.extension.sendMessage({}, function(response) {
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
 
+
+
+		// Editing the dom and manipulating the youtube
+		var ytID = window.location.search;
+		ytID = ytID.substring(2);
+
+		var ytEmbed = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'+ ytId +'" frameborder="0" allowfullscreen></iframe>';
+
+		var mmvStart = function(){
+
+			$( "#body-container" ).append(ytEmbed);
+
+
+		};
+
 	}
 	}, 10);
 });
