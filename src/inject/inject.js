184e655436a461324d5ed3e058b4076404084388
Matt Burman <matt@mattburman.com>
2016-08-20 16:23:38 +0100

    some es6 style

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,57 +1,52 @@
 const config = {
-  host: '//twilio.mattburman.com'
+  host: '//twilio.mattburman.com',
 };
 
-chrome.extension.sendMessage({}, function(response) {
-  var readyStateCheckInterval = setInterval(function() {
-    if (document.readyState === "complete") {
+chrome.extension.sendMessage({}, response => {
+  const readyStateCheckInterval = setInterval(() => {
+    if (document.readyState === 'complete') {
+      let client;
+
       clearInterval(readyStateCheckInterval);
 
-      $('head').append("<script type='text/javascript' src='https://media.twiliocdn.com/sdk/js/common/v0.1/twilio-common.min.js'>");
-      $('head').append("<script type='text/javascript' src='https://media.twiliocdn.com/sdk/js/sync/v0.2/twilio-sync.min.js'>");
-      $('head').append("<style>.wwwatch-buttons{background-color:white; width: 100%; height: 80px; margin-bottom: 10px; box-shadow: 0 1px 2px rgba(0,0,0,.1);}</style>")
+      $('head').append('<script type=\'text/javascript\' src=\'https://media.twiliocdn.com/sdk/js/common/v0.1/twilio-common.min.js\'>');
+      $('head').append('<script type=\'text/javascript\' src=\'https://media.twiliocdn.com/sdk/js/sync/v0.2/twilio-sync.min.js\'>');
+      $('head').append('<style>.wwwatch-buttons{background-color:white; width: 100%; height: 80px; margin-bottom: 10px; box-shadow: 0 1px 2px rgba(0,0,0,.1);}</style>');
 
-      var client;
+      fetchAccessToken(authData => {
+        client = new Twilio.Sync.Client(new Twilio.AccessManager(authData.token));
 
-      fetchAccessToken(function(data) {
-        client = new Twilio.Sync.Client(new Twilio.AccessManager(data.token));
+        console.log('You are: ' + client.accessManager.identity);
 
-        console.log("You are: " + client.accessManager.identity);
-
-        client.document("vid").then(function (doc) {
-          doc.mutate(function(data) {
-            $('video')[0].currentTime = data.time; //TODO: plus time since updated
-
+        client.document('vid').then(doc => {
+          doc.mutate(data => {
+            $('video')[0].currentTime = data.time; // TODO: plus time since updated
             return data;
           });
 
-          doc.on("updated",function(data) {
+          doc.on('updated', data => {
             $('video')[0].currentTime = data.time;
           });
         });
 
-        $('video')[0].onplay = function() {
-          client.document('vid').then(function(doc) {
-            doc.mutate(function(data) {
+        $('video')[0].onplay = () => {
+          client.document('vid').then(doc => {
+            doc.mutate(data => {
               data.time = $('video')[0].currentTime;
-
               return data;
             });
           });
         };
       });
 
-      //Button Controlls
-      $('.watch-main-col').prepend('<div class="wwwatch-buttons"><h1>Test</h1></div>');
-      $('.watch-sidebar').prepend('<div class="wwwatch-buttons"><h1>Test</h1></div>');
-
-
+      // Button Controlls
+      $('.watch-main-col').prepend('<div class=\'wwwatch-buttons\'><h1>Test</h1></div>');
+      $('.watch-sidebar').prepend('<div class=\'wwwatch-buttons\'><h1>Test</h1></div>');
     }
   }, 10);
 });
 
-function fetchAccessToken (cb) {
-  $.getJSON(config.host + '/token', { device: 'browser' }, data => {
-    cb(data);
-  });
+function fetchAccessToken(cb) {
+  $.getJSON(config.host + '/token', { device: 'browser' }, cb);
 }
+
